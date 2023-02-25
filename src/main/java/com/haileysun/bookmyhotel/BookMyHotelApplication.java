107ce317f5f50f0300bef277ad51f25c4b62cc1f
Xiaowen Sun <xiaowensun0212@gmail.com>
2023-02-25 13:09:58 -0800

    Initial commit

---
+++ src/main/java/com/haileysun/bookmyhotel/BookMyHotelApplication.java
@@ -0,0 +1,13 @@
+package com.haileysun.bookmyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haileysun/bookmyhotel/BookMyHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haileysun.bookmyhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
