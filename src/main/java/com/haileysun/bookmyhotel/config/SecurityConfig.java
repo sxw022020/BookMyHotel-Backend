508b3776ffe94cee6ec2cda5337da40b133fe130
Xiaowen Sun <xiaowensun0212@gmail.com>
2023-02-27 19:22:47 -0800

    update configuration

--- src/main/java/com/haileysun/bookmyhotel/config/SecurityConfig.java
+++ src/main/java/com/haileysun/bookmyhotel/config/SecurityConfig.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@EnableWebSecurity
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
