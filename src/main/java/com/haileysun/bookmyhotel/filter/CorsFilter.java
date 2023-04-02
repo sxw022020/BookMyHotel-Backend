78745805f05c48fb355f021c3cab3ec4aa481287
Xiaowen Sun <xiaowensun0212@gmail.com>
2023-04-02 11:43:53 -0700

    add CorsFilter to support CORS

---
+++ src/main/java/com/haileysun/bookmyhotel/filter/CorsFilter.java
@@ -0,0 +1,45 @@
+package com.haileysun.bookmyhotel.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * To Enable CORS on the backend,
+ * we need to handle the CORS *preflight requests* (see Notes.md) and
+ * return the dedicated CORS header to the frontend.
+ */
+@Component
+// the filter will be executed before all other filters that have not been assigned a specific order or have a lower precedence
+@Order(Ordered.HIGHEST_PRECEDENCE)
+// ensures that the filter is *applied only once per request*
+public class CorsFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // allows any domain to access the resources
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        // allow POST, GET, OPTIONS, and DELETE HTTP methods for cross-origin requests
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+        // allow the "Authorization" and "Content-Type" headers in cross-origin requests.
+        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(response.SC_OK);
+        // If the request is not an OPTIONS request,
+        // the filter chain is continued by calling filterChain.doFilter(request, response),
+        // which allows further processing of the request.
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
